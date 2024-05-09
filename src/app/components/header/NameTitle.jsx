67e6e0cf8a74a720614397e2fd0e6983334bc3a3
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-05-09 19:13:51 +0300

    Some global styles migrated

--- src/app/components/header/NameTitle.jsx
+++ src/app/components/header/NameTitle.jsx
@@ -7,7 +7,7 @@ const NameTitle = () => {
     <a className={classes.nameTitle} href="#home">
       <div className={classes["nameTitle__container"]}>
         <span className={`${classes["nameTitle__inner"]} ${classes["nameTitle__inner--first"]} mr-[7px]`}>{REACT_APP_FIRST_NAME}</span>
-        <span className={`${classes["nameTitle__inner"]} ${classes["nameTitle__inner--second"]}`}>{REACT_APP_LAST_NAME}</span>
+        <span className={`${classes["nameTitle__inner"]} ${classes["nameTitle__inner--second"]} font-bold`}>{REACT_APP_LAST_NAME}</span>
       </div>
     </a>
   )
