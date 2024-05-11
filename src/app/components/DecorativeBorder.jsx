0f22e41e095490c64a9d923c38aefef4ebede532
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-05-11 05:16:49 +0300

    Dropdown arrow and border comps as styled components

--- src/app/components/DecorativeBorder.jsx
+++ src/app/components/DecorativeBorder.jsx
@@ -1,5 +1,31 @@
-const DecorativeBorder = (props) => {
-  return <div className={`decorative-border decorative-border--${props.location}`}></div>
+import styled from "styled-components"
+
+const StyledDecorativeBorder = styled.div`
+  background-color: var(--color-primary);
+  height: 12vh;
+  pointer-events: none;
+  position: absolute;
+  width: 100%;
+  z-index: 3;
+
+  @media only screen and (max-width: 1199px) {
+    height: 6vh;
+  }
+
+  &.top {
+    clip-path: polygon(0 0, 100% 0, 100% 0%, 0 100%);
+    top: 0;
+  }
+
+  &.bottom {
+    clip-path: polygon(0 0, 100% 0, 100% 0, 0 100%);
+    transform: rotate(-180deg);
+    bottom: 0;
+  }
+`
+
+const DecorativeBorder = ({ location }) => {
+  return <StyledDecorativeBorder className={location}></StyledDecorativeBorder>
 }
 
 export default DecorativeBorder
--- src/app/components/DownArrow.jsx
+++ src/app/components/DownArrow.jsx
@@ -1,18 +1,64 @@
 import Image from "next/image"
-import downArrow from "../../assets/down-arrow.svg"
+import arrowDownSvg from "../../assets/down-arrow.svg"
+
+import styled from "styled-components"
+
+const StyledDownArrow = styled.a`
+  position: absolute;
+  bottom: 20vh;
+  z-index: 3;
+  left: 50%;
+
+  @media only screen and (min-width: 590px)  {
+    bottom: 12vh;
+  }
+
+  animation-name: down-arrow-animate;
+  animation-iteration-count: infinite;
+  animation-duration: 2s;
+
+  .down-arrow__image {
+    transform: translateX(-50%);
+    width: 30px;
+    height: 30px;
+
+    @media only screen and (min-width: 590px)  {
+      width: 50px;
+      height: 50px;
+    }
+  }
+
+  @keyframes down-arrow-animate {
+    0% {
+      transform: translateY(0);
+    }
+
+    40% {
+      transform: translateY(25%);
+    }
+
+    50% {
+      transform: translateY(25%);
+    }
+
+    100% {
+      transform: translateY(0);
+    }
+  }
+`
 
 const DownArrow = () => {
   return (
-    <a href="#about" className="down-arrow" data-scroll-to>
+    <StyledDownArrow href="#about" className="down-arrow" data-scroll-to>
       <Image
         className="down-arrow__image animate__animated animate__delay-1s"
         data-scroll
         data-scroll-repeat="false"
         data-scroll-class="animate__fadeIn"
-        src={downArrow}
+        src={arrowDownSvg}
         alt=""
       />
-    </a>
+    </StyledDownArrow>
   )
 }
 
