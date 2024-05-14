3e7e0930a4f475c05ab786cfa246e1df5493f9b2
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-05-14 14:21:32 +0100

    Placeholders blog and projects skeletons removed

--- src/app/blog/[id]/page.jsx
+++
@@ -1,5 +0,0 @@
-const Post = ({ params }) => {
-  return <h1>blog post {params.id}</h1>;
-};
-
-export default Post;
--- src/app/projects/[id]/page.jsx
+++
@@ -1,5 +0,0 @@
-const Project = ({params}) => {
-  return <h1>project {params.id}</h1>;
-}
-
-export default Project
