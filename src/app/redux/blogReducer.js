546dbddfbcc5a5be415bf78ad5d902bb845eef50
Georgi Danchev <38672257+georgidanchev@users.noreply.github.com>
2024-05-22 21:25:02 +0100

    Fetching blog data via the blog reducer

--- src/app/redux/blogReducer.js
+++ src/app/redux/blogReducer.js
@@ -1,22 +1,44 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-import useFetch, { getAllBlogPosts } from "../hooks/useFetch";
-const { isFetching, error, fetchedData } = getAllBlogPosts;
+export const fetchBlogData = createAsyncThunk("api/blogPosts", () => {
+  return fetch("/api/blogPosts")
+    .then((res) => res.json())
+    .then((data) => data);
+});
 
+const initialState = {
+  loading: false,
+  data: [],
+  error: "",
+};
 
 export const blogSlice = createSlice({
   name: "blog",
-  initialState: {
-    data: [],
-  },
-  reducers: {
-    setBlogData: (state, action) => {
-      const newData = action.payload;
-      state.data = newData;
-    },
+  initialState,
+  // reducers: {
+  //   setBlogData: (state, action) => {
+  //     const newData = action.payload;
+  //     state.data = newData;
+  //   },
+  // },
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchBlogData.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchBlogData.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload
+      state.error = ""
+    });
+    builder.addCase(fetchBlogData.rejected, (state, action) => {
+      state.loading = false;
+      state.data = []
+      state.error = action.error.message
+    });
   },
 });
 
-export const blogActions = blogSlice.actions
+// export const blogActions = blogSlice.actions;
 
-export default blogSlice.reducer
+export default blogSlice.reducer;
--- src/app/sections/BlogSection.jsx
+++ src/app/sections/BlogSection.jsx
@@ -2,25 +2,20 @@ import BlogPost from "../components/BlogPost";
 import SectionTitle from "../components/SectionTitle";
 import sectionClasses from "../styles/shared/Sections.module.scss";
 import blogClasses from "./BlogSection.module.scss";
-
-// import { getBlogData } from "../redux/blogReducer";
 import { InView } from "react-intersection-observer";
 import { setSectionInView } from "../redux/navReducer";
 import { useDispatch, useSelector } from "react-redux";
-import useFetch, { getAllBlogPosts } from "../hooks/useFetch";
-// import { blogActions } from "../redux/blogReducer";
+import { useEffect } from "react";
+import { fetchBlogData } from "../redux/blogReducer";
 
 const Blog = () => {
+  const blogState = useSelector((state) => state.blog);
+
   const dispatch = useDispatch();
 
-
-  // dispatch(blogActions.setBlogData(fetchedData));
-
-  let blogPosts = [];
-
-  fetchedData.forEach((blog_post) => {
-    blogPosts.push(<BlogPost key={blog_post.id} blog_post={blog_post} />);
-  });
+  useEffect(() => {
+    dispatch(fetchBlogData());
+  }, [dispatch]);
 
   const isInView = (inView) => {
     if (inView) {
@@ -41,7 +36,16 @@ const Blog = () => {
         className={`${sectionClasses["section-width"]} ${sectionClasses["section-width--padding"]} ${blogClasses["blog"]}`}
       >
         <SectionTitle subtitle="latest posts" title="My Blog Posts" />
-        <div className={`${blogClasses["blog__content"]}`}>{blogPosts}</div>
+        <div className={`${blogClasses["blog__content"]}`}>
+          {blogState.loading && <p>Loading...</p>}
+
+          {!blogState.loading && blogState.error ? <p>Error: {blogState.error}</p> : null}
+
+          {!blogState.loading && blogState.data
+            ? blogState.data.map((blogPost) => <BlogPost key={blogPost.id} blog_post={blogPost} />)
+            : null}
+          {/* {blogPosts} */}
+        </div>
       </div>
     </InView>
   );
